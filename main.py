5f20af29e2a01083cdd2b97d72e3ea82dca0780a
Gladwin George <gladwingeorge189@gmail.com>
2024-03-11 16:55:28 +0530

    added logout feature

--- main.py
+++ main.py
@@ -265,6 +265,12 @@ def user():
     # If the user is not logged in, redirect to the login page
     return redirect(url_for('login'))
 
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
